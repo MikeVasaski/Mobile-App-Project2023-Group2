d9628862a2598b5c320767a7f8d9b896fa934827
mikeJT <jeerasaktarat@gmail.com>
2023-02-27 12:50:23 +0700

    first init 3 page created

---
+++ mobilpgroup2/android/app/src/main/kotlin/com/example/mobilpgroup2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobilpgroup2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mobilpgroup2/lib/pages/treesmap.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TreesPage extends StatefulWidget {
+  const TreesPage({Key? key}) : super(key: key);
+
+  @override
+  _TressPageState createState() => _TreesPageState();
+}
+
+class _TressPageState extends State<TreesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
